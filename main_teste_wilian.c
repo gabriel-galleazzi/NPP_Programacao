4b02fb5418ab429549e881a516d21232de6a18bb
gabriel-galleazzi <galleazzi_1@hotmail.com>
2022-07-23 17:14:04 -0300

    Novo_nome

---
+++ main_teste_wilian.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main (void)
+{
+    printf("Ola Mundo!\n");
+    printf("Ola Release!\n");
+
+    printf("Eu sou o programador 1\n");
+
+    printf("Teste 1\n");
+    printf("Ola eu sou o programador 2\n");
+
+    printf("Teste 2");
+
+    return 0;
+}
