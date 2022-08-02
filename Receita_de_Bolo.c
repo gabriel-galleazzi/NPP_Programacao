97cf1eea889ecb0c9fa80fa1db931f6b9605f7e1
gabriel-galleazzi <galleazzi_1@hotmail.com>
2022-08-02 13:27:11 -0300

    passo_2_COMPLETO

--- Receita_de_Bolo.c
+++ Receita_de_Bolo.c
@@ -6,9 +6,11 @@ int main()
    numero_de_passos = 1;
    numero_de_passos_ideal = 15;
 
-   printf ("Inicio da receita:\n");
-   printf ("%d° Passo da receita\n", numero_de_passos);
+   printf ("\n\nInicio da receita:\n");
+   printf ("\nPasso %d da receita\n\n", numero_de_passos);
    printf ("Preparar os ingredientes do bolo:\n");
-   printf ("2 xicaras (cha) de acucar\n3 xicaras (cha) de farinha de trigo\n4 colheres (sopa) de margarina\n3 ovos\nleite\nfermento\n ");
+   printf ("acucar\nfarinha de trigo\nmargarina\novos\nleite\nfermento\n\n ");
+   printf ("Passo %d da receita\n\n", numero_de_passos=numero_de_passos+1);
+   printf ("Agora que estamos com os ingredientes vamos adicionalos a um recepiciente:\n");
    printf ("2 xicaras (cha) de acucar\n3 xicaras (cha) de farinha de trigo\n4 colheres (sopa) de margarina\n3 ovos\n1 e 1/2 xicara(cha) de leite\n1 colher de (sopa) bem cheia de fermento\n ");
 }
