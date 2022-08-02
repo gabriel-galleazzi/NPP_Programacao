c4bb5e2347b9128fa92ffa56156396423f0b3e57
gabriel-galleazzi <galleazzi_1@hotmail.com>
2022-08-02 13:07:07 -0300

    Receita
    adicionando o (.c) fazendo uma receita de bolo ^^

---
+++ Receita_de_Bolo.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+   unsigned char numero_de_passos; 
+   unsigned char numero_de_passos_ideal;
+   numero_de_passos = 1;
+   numero_de_passos_ideal = 15;
+
+   printf ("Inicio da receita:\n");
+   printf ("%d° Passo da receita\n", numero_de_passos);
+   printf ("Preparar os ingredientes do bolo:\n");
+   printf ("2 xicaras (cha) de acucar\n3 xicaras (cha) de farinha de trigo\n4 colheres (sopa) de margarina\n3 ovos\nleite\nfermento\n ");
+   printf ("2 xicaras (cha) de acucar\n3 xicaras (cha) de farinha de trigo\n4 colheres (sopa) de margarina\n3 ovos\n1 e 1/2 xicara(cha) de leite\n1 colher de (sopa) bem cheia de fermento\n ");
+}
