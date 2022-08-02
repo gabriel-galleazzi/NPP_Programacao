95df66638234c399955b049092b8c4420adf01d3
gabriel-galleazzi <galleazzi_1@hotmail.com>
2022-08-02 14:30:44 -0300

    passo_15

--- Receita_de_Bolo.c
+++ Receita_de_Bolo.c
@@ -53,7 +53,7 @@ int main()
    printf ("e jogue farinha em cima da forma untada\n");
 
    printf ("\n- Passo %d da receita\n", numero_de_passos=numero_de_passos+1);
-   printf ("");
+   printf ("Despeje a massa na forma untada\n");
 
    printf ("\n- Passo %d da receita\n", numero_de_passos=numero_de_passos+1);
    printf ("");
