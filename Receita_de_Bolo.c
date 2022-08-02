77d8c9c01efd64c8ff0264cf85163b398f5ed651
gabriel-galleazzi <galleazzi_1@hotmail.com>
2022-08-02 14:00:42 -0300

    passos_add

--- Receita_de_Bolo.c
+++ Receita_de_Bolo.c
@@ -10,7 +10,25 @@ int main()
    printf ("\nPasso %d da receita\n\n", numero_de_passos);
    printf ("Preparar os ingredientes do bolo:\n");
    printf ("acucar\nfarinha de trigo\nmargarina\novos\nleite\nfermento\n\n ");
+
+   printf ("Agora que estamos com os ingredientes vamos separar um recepiciente e adicionar-los:\n");
    printf ("Passo %d da receita\n\n", numero_de_passos=numero_de_passos+1);
-   printf ("Agora que estamos com os ingredientes vamos adicionalos a um recepiciente:\n");
-   printf ("2 xicaras (cha) de acucar\n3 xicaras (cha) de farinha de trigo\n4 colheres (sopa) de margarina\n3 ovos\n1 e 1/2 xicara(cha) de leite\n1 colher de (sopa) bem cheia de fermento\n ");
+   printf ("2 xicaras (cha) de acucar\n\n ");
+
+   printf ("Passo %d da receita\n\n", numero_de_passos=numero_de_passos+1);
+   printf ("3 xicaras (cha) de farinha de trigo\n");
+
+   printf ("Passo %d da receita\n\n", numero_de_passos=numero_de_passos+1);
+   printf ("4 colheres (sopa) de margarina\n");
+   
+   printf ("Passo %d da receita\n\n", numero_de_passos=numero_de_passos+1);
+   printf ("3 ovos\n");
+
+   printf ("Passo %d da receita\n\n", numero_de_passos=numero_de_passos+1);
+   printf ("1 e 1/2 xicara(cha) de leite");
+   
+   printf ("Passo %d da receita\n\n", numero_de_passos=numero_de_passos+1);
+   printf ("1 colher de (sopa) bem cheia de fermento\n");
+   
+   return 0;
 }
