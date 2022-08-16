a155a583a2a110687a194ec987ddf6255711a582
gabriel-galleazzi <galleazzi_1@hotmail.com>
2022-08-16 10:42:54 -0300

    resolvendo o desafio

--- Desafio beecrowd - 2342.c
+++ Desafio beecrowd - 2342.c
@@ -9,12 +9,20 @@ void main (void)
     // r = resultado
     char c; // + ou *
    
+    printf ("Digite o valor de overflow desejado entre (1 a 500.00):\n"); 
     scanf("%d", &overflow);
     
     //entrada de P e Q e C
-    scanf ("%d %d %c", &p, &q, &c);
+    printf ("Digite o valor de P (0 a 1000):\n");
+    scanf ("%d", &p);
     
-    
+    printf ("Digite o valor de Q (0 a 1000):\n");
+    scanf ("%d", &q);
+
+    printf ("Digite + ou *:\n");
+    c = getche();
+    //printf ("%c\n", c);
+
     if (c=='+')
        r = p+q;
     else 
@@ -26,7 +34,42 @@ void main (void)
        printf ("ERROR"); 
 
     if (overflow<r) 
+    printf ("\nVoce teve um OVERFLOW\n"); 
+    else
+    printf ("\nSeus valores estao OK\n");
+}
+
+/*
+#include<stdio.h>
+
+void main (void)
+{
+    int overflow, p, q, r;
+   
+    char c; // + ou *
+   
+   //entrada overflow
+    scanf("%d", &overflow);
+    
+    //entrada de P e Q e C
+    scanf ("%d %d %c", &p, &q, &c);
+    
+    //logica para a escolha de + ou *
+    if (c=='+')
+       r = p+q;
+    else 
+      if (c=='*')
+      {
+        r = p*q;
+      }
+      else
+       printf ("ERROR"); 
+
+    //if para saida final
+    if (overflow<r) 
     printf ("OVERFLOW\n"); 
     else
     printf ("OK\n");
+    return 0;
 }
+*/
--- Exercicios_aula_NPP.c
+++ Exercicios_aula_NPP.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 int main (void)
 {
-    printf ("Hello World !\n");
+    /*printf ("Hello World !\n");
     printf ("FATEC SOROCABA NPP\n");
-    printf ("Gabriel Galleazzi\n");
-    
+    printf ("Gabriel Galleazzi\n");*/
+    char c;
+    scanf ("%c", &c);
+    printf ("exiba o valor de char: %c", c);
+
     return 0;
 }
